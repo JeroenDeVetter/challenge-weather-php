055de103551ca007d292727887ecd4089a2e3c6f
jeroen <devetterjeroen@gmail.com>
2019-12-03 16:56:23 +0100

    all done for today

--- chellenge.php
+++ chellenge.php
@@ -1,27 +1,89 @@
 <?php
-
-
-  function click() {
-
-      $json = json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/forecast?'+ $_GET['subject']  +'&APPID=d02ba4169b2ac4f0d179b1e84c341147'));
-  }
-
-?>
-<?php
-
-?>
-
-
+      $input = $_GET['input'];
+      $json = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/forecast?q=$input&units=metric&APPID=d02ba4169b2ac4f0d179b1e84c341147"));
+function validateDate($date, $format = 'Y-m-d H:i:s')
+{
+    $d = DateTime::createFromFormat($format, $date);
+    return $d && $d->format($format) == $date;
+}
+      ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/index.css">
     <title>Document</title>
 </head>
 <body>
-  <input type="button" name="Release" onclick="document.write('<?php hello() ?>');" value="Click to Release">
-  <button></button>
+  <h1>Weather App Jeroen PHP</h1>
+  <form action="" method="get">
+    <input type="text" name="input" value="Mickey"><br>
+    <button type="submit" value="Submit">Click here</button>
+</form>
+
+<div id="weather">
+    <h1><?php echo $json->city->name?></h1>
+ <?php
+     for ($i = 0; $i < count($json->list) ; $i += 8) {
+         echo '<div class ="card">';
+
+         $date = date(l ,strtotime($json->list[$i]->dt_txt ));
+           if(validateDate($date) == true) {
+               echo '<h2>';
+               echo 'Today';
+               echo '</h2>';
+           } else {
+               echo '<h2>';
+               echo $date;
+               echo '</h2>';
+           }
+         echo '<p class="description">';
+         echo $json->list[$i]->weather[0]->description;
+         echo '</p>';
+
+
+
+         echo "<p class='percent_cloudDek'>";
+         echo $json->list[$i]->clouds->all;
+         echo '% clouds in the sky';
+         echo "</p>";
+
+         echo "<p class='max_temp'>";
+         echo 'Max Temp = ';
+         echo $json->list[$i]->main->temp_max;
+         echo '°C';
+         echo '</p>';
+
+         echo "<p class='min_temp'>";
+         echo 'Min Temp = ';
+         echo $json->list[$i]->main->temp_min;
+         echo '°C';
+         echo '</p>';
+
+         echo "<p class='pressure'>";
+         echo 'Pressure = ';
+         echo $json->list[$i]->main->pressure;
+         echo "</p>";
+
+         echo "<p class='wind-speed'>";
+         echo 'KMPH ';
+         echo intval($json->list[$i]->wind->speed * 3.6);
+         echo "</p>";
+
+         echo '<p class="wind-dir">';
+         echo 'Wind direction : ';
+         echo $json->list[$i]->wind->deg;
+         echo ' °';
+         echo '</p>';
+
+         echo '</div>';
+     }
+
+
+    ?>
+
+</div>
 </body>
 </html>
